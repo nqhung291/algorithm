1d8bb71d827b073d80b78cbf46377b01d4930b26
nqhung291 <nqhung291@gmail.com>
2020-11-07 08:59:11 +0700

    not done yet

---
+++ divide-conquer/EKO/solve.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m, a[1000001];
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> n >> m;
+  for (int i=1;i<=n;i++) {
+    cin >> a[i];
+  }
+
+  
+
+  return 0;
+}
